007dc83f1be603a508574a0a1f4753ffde2c4dde
ranyery <50641030+ranyery@users.noreply.github.com>
2020-02-15 13:37:49 -0300

    Adição de novas funções

--- 01 - Lista Telefônica/js/script.js
+++ 01 - Lista Telefônica/js/script.js
@@ -4,13 +4,30 @@ angular.module('listaTelefonica')
   .controller('listaTelefonicaCtrl', ['$scope', function ($scope) {
     $scope.app = "Lista Telefônica";
     $scope.contatos = [
-      { nome: 'Pedro', telefone: '99998888' },
-      { nome: 'Ana', telefone: '99998877' },
-      { nome: 'Maria', telefone: '99998866' },
-      { nome: 'Ranyery', telefone: '940028922' }
+      { nome: 'Pedro', telefone: '99998888', cor: 'red' },
+      { nome: 'Ana', telefone: '99998877', cor: 'green' },
+      { nome: 'Maria', telefone: '99998866', cor: 'blue' },
+      { nome: 'Ranyery', telefone: '940028922', cor: 'yellow' }
+    ];
+    $scope.operadoras = [
+      {nome: 'Oi', codigo: 14, categoria: 'celular'},
+      {nome: 'Vivo', codigo: 15, categoria: 'celular'},
+      {nome: 'Tim', codigo: 41, categoria: 'celular'},
+      {nome: 'GVT', codigo: 25, categoria: 'Fixo'},
+      {nome: 'Embratel', codigo: 21, categoria: 'Fixo'},
     ];
     $scope.adicionarContato = function (contato) {
       $scope.contatos.push(angular.copy(contato));
       delete $scope.contato;
     };
+    $scope.apagarContatos = function (contatos) {
+      $scope.contatos = contatos.filter(function(contatos) {
+        return !contatos.selecionado
+      });
+    };
+    $scope.isContatoSelecionado = function(contatos) {
+        return contatos.some(function(contato) {
+          return contato.selecionado;
+      });
+    }
   }]);
